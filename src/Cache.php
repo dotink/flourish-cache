419440514a801c8ae67e0d86e556ba311549bdb5
Matthew J. Sahagian <msahagian@dotink.org>
2015-02-18 21:06:34 -0800

    Copyright fixup

--- src/Cache.php
+++ src/Cache.php
@@ -3,13 +3,13 @@
 	/**
 	 * A simple caching interface for multiple backends
 	 *
-	 * @copyright  Copyright (c) 2007-2011 Will Bond, others
-	 * @author     Will Bond           [wb]  <will@flourishlib.com>
-	 * @author     Matthew J. Sahagian [mjs] <msahagian@dotink.org>
+	 * @copyright Copyright (c) 2007-2015 Will Bond, Matthew J. Sahagian, others
+	 * @author Will Bond [wb] <will@flourishlib.com>
+	 * @author Matthew J. Sahagian [mjs] <msahagian@dotink.org>
 	 *
-	 * @license    Please see the LICENSE file at the root of this distribution
+	 * @license Please reference the LICENSE.md file at the root of this distribution
 	 *
-	 * @package    Flourish
+	 * @package Flourish
 	 */
 	class Cache
 	{
